c32e0c6d25798b4b6308ff1e7ce5d1f7fde6a511
Juergen Weigert <jnweiger@gmail.com>
2021-11-25 01:05:04 +0100

    Photos and testbed hardware svgs added.

--- esp32/knightrider-ota-jw/knightrider-ota-jw.ino
+++ esp32/knightrider-ota-jw/knightrider-ota-jw.ino
@@ -189,7 +189,7 @@ unsigned int knightrider_pos_ticker[FB_N_ROWS] = {
 };
 
 const unsigned int knightrider_ticks_per_pos[FB_N_ROWS] = {
-  5,2,3,4,6,8,12,16,24,32,
+  3,2,3,4,6,8,12,16,24,32,
   48,64,96,128,192,256,384,512,768,1024,
   1536,2048,3072,4096,6144,8192,12288,16384,24576,32768
 };
